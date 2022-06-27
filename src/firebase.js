08666704a6e7d554dc57fd80596ed6154bc365ea
DukeThor <ducthotran2010@gmail.com>
2022-06-27 19:26:49 +0700

    Fixed blank screen

--- src/firebase.js
+++ src/firebase.js
@@ -10,7 +10,7 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  apiKey: "AIzaSyC9RHymEIOZ9oY6bgmf-0IpWF1JakfTK-g",
   authDomain: "ticket-vleague.firebaseapp.com",
   projectId: "ticket-vleague",
   storageBucket: "ticket-vleague.appspot.com",
