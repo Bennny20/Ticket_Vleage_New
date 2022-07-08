0c2e5e3783caa00a81603b1482c9e34cbe7d83f7
Anhncd <anhncd1708@gmail.com>
2022-07-08 21:10:47 +0700

    add LoginWithGoogle function

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,7 +3,9 @@ import "./login.scss";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
 import { useNavigate } from "react-router-dom";
-import {AuthContext} from "../../context/AuthContext"
+import { AuthContext } from "../../context/AuthContext"
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
 
 const Login = () => {
   const [error, setError] = useState(false);
@@ -12,8 +14,9 @@ const Login = () => {
 
   const navitage = useNavigate()
 
-  const {dispatch} = useContext(AuthContext)
+  const { dispatch } = useContext(AuthContext)
 
+  //sign in bằng account được cấp qua firebase
   const handleLogin = (e) => {
     e.preventDefault();
 
@@ -22,7 +25,7 @@ const Login = () => {
         // Signed in
         const user = userCredential.user;
         console.log(user);
-        dispatch({type:"LOGIN", payload:user})
+        dispatch({ type: "LOGIN", payload: user })
         navitage("/")
       })
       .catch((error) => {
@@ -30,22 +33,56 @@ const Login = () => {
       });
   };
 
+  //sign in with google firebase ====> chưa check role admin => ai cũng login được
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        console.log(user);
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
+
+
+
   return (
     <div className="login">
       <form onSubmit={handleLogin}>
         <input
           type="email"
           placeholder="email"
-          onChange={e=> setEmail(e.target.value)}
+          onChange={e => setEmail(e.target.value)}
         />
         <input
           type="password"
           placeholder="password"
-          onChange={e=> setPassword(e.target.value)}
+          onChange={e => setPassword(e.target.value)}
         />
         <button type="submit">Login</button>
+
+        <div class="google-btn" onClick={signInWithGoogle} >
+          <div class="google-icon-wrapper">
+            <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" />
+          </div>
+          <p class="btn-text"><b>Sign in with google</b></p>
+        </div>
         {error && <span>Wrong email or password!</span>}
       </form>
+
     </div>
   );
 };
