2cbd3b3e15c38ed93eacc1bf111e9a0ea412fd60
Anhncd <anhncd1708@gmail.com>
2022-07-11 15:36:40 +0700

    Fix login logout

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -76,9 +76,9 @@ const Login = () => {
         />
         <button type="submit">Login</button>
 
-        <div class="google-btn" onClick={signInWithGoogle} >
-          <div class="google-icon-wrapper">
-            <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" />
+        <div className="google-btn" onClick={signInWithGoogle} >
+          <div className="google-icon-wrapper">
+            <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" />
           </div>
           <p class="btn-text"><b>Sign in with google</b></p>
         </div>
