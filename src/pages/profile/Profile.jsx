0c6b932bd46e914ec13330cdda4942d1acdaef98
Anhncd <anhncd1708@gmail.com>
2023-03-06 02:07:04 +0700

    chỉnh tí

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -13,47 +13,50 @@ const Profile = () => {
 
 
   return (
-    <div className="single">
-      <div className="singleContainer">
-        <Navbar />
-        <div className="top">
-          <div className="left">
-            {/* <div className="editButton">Edit</div> */}
-            <h1 className="title">Information</h1>
-            <div className="item">
-              <img
-                src="https://i.pinimg.com/736x/c6/e5/65/c6e56503cfdd87da299f72dc416023d4.jpg"
-                alt=""
-                className="itemImg"
-              />
-              <div className="details">
-                <h1 className="itemTitle">{currentUser.username}</h1>
-                <div className="detailItem">
-                  <span className="itemKey">Email:</span>
-                  <span className="itemValue">{currentUser.email}</span>
-                </div>
-                <div className="detailItem">
-                  <span className="itemKey">ROLE:</span>
-                  {currentUser.isAdmin ? <span className="itemValue">ADMIN</span> : <span className="itemValue">USER</span>}
+    <div><Navbar />
+      <div className="single">
 
-                </div>
-                <div className="detailItem">
-                  <span className="itemKey">CREATED AT:</span>
-                  <span className="itemValue">
-                    {moment(currentUser.createdAt).format('DD/MM/YYYY -  h:mm a')}
-                  </span>
-                </div>
-                {/* <div className="detailItem">
+        <div className="singleContainer">
+
+          <div className="top">
+            <div className="left">
+              {/* <div className="editButton">Edit</div> */}
+              <h1 className="title">Information</h1>
+              <div className="item">
+                <img
+                  src="https://i.pinimg.com/736x/c6/e5/65/c6e56503cfdd87da299f72dc416023d4.jpg"
+                  alt=""
+                  className="itemImg"
+                />
+                <div className="details">
+                  <h1 className="itemTitle">{currentUser.username}</h1>
+                  <div className="detailItem">
+                    <span className="itemKey">Email:</span>
+                    <span className="itemValue">{currentUser.email}</span>
+                  </div>
+                  <div className="detailItem">
+                    <span className="itemKey">ROLE:</span>
+                    {currentUser.isAdmin ? <span className="itemValue">ADMIN</span> : <span className="itemValue">USER</span>}
+
+                  </div>
+                  <div className="detailItem">
+                    <span className="itemKey">CREATED AT:</span>
+                    <span className="itemValue">
+                      {moment(currentUser.createdAt).format('DD/MM/YYYY -  h:mm a')}
+                    </span>
+                  </div>
+                  {/* <div className="detailItem">
                   <span className="itemKey">Country:</span>
                   <span className="itemValue">USA</span>
                 </div> */}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="bottom">
-          <h1 className="title">Last Transactions</h1>
-          <Datatable />
+          <div className="bottom">
+            <h1 className="title">Last Transactions</h1>
+            <Datatable />
+          </div>
         </div>
       </div>
     </div>
