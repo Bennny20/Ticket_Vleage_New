63e416038db3177eb3469fb5890eaec5a6876bae
Pham Ngoc Long <94893379+LongPN-BE@users.noreply.github.com>
2023-03-06 21:51:10 +0700

    Axios Config Cookie Authen

--- src/AxiosConfig.js
+++ src/AxiosConfig.js
@@ -1,11 +1,17 @@
 import axios from 'axios';
 // var path = "https://jsonplaceholder.typicode.com/";
 var path = "http://localhost:8800/api/";
-
+const access_token = localStorage.getItem("access_token");
 const instance = axios.create({
   baseURL: path,
   withCredentials: false,
+  Cookie : `access_token=${access_token}`,
   headers: {
+    Accept: "application/json",
+    Cookie: `access_token=${access_token}`,
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Expose-Headers': '*',
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
   }
